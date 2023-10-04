ec319a5305bf478c10d2673d13dcd1f85202ba60
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-10-04 13:49:35 +0530

    new changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,6 +13,7 @@ import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
+import cginfinity from "./cginfinty.png"
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
@@ -59,4 +60,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  cginfinity,
 };
