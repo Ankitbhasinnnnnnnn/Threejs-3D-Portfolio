0006a768f17e2b01670e5af731f70a68c21a0c1b
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-10-05 13:44:33 +0530

    x

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Ankit kumar</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />
-            interfaces and web applications
+          Coding in multiple dimensions<br className="sm:block hidden" />
+             front, back, and beyond.
           </p>
         </div>
       </div>
